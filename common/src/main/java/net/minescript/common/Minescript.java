2ebf0cf12df0127c2553dce2c497eaee8f5d4850
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2025-08-10 22:16:59 +0200

    Update Minescript.java

--- common/src/main/java/net/minescript/common/Minescript.java
+++ common/src/main/java/net/minescript/common/Minescript.java
@@ -1662,7 +1662,8 @@ public class Minescript {
             .filter(s -> s.startsWith(command))
             .collect(Collectors.toList());
       } else if (command.startsWith("q") && command.contains(" ")) {
-        return config.scriptConfig().findCommandPrefixMatches(command.replaceAll(" ", "--")).stream()
+        String[] nums = command.split("(?= -?\d+(\.\d+)?([eE][+-]?\d+)?) ", -1);
+        return config.scriptConfig().findCommandPrefixMatches(command.replaceAll(" -?\d+(\.\d+)?([eE][+-]?\d+)?", "---num-").replaceAll(" ", "--")).stream()
             .sorted()
             .map(c -> c.replaceAll("--", " "))
             .filter(s -> s.startsWith(command))
